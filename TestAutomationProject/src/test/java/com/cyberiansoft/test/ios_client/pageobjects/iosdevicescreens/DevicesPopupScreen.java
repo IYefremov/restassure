0fc84dd054700120efa38a50d3a4ce46a4cc30df
Okramar <olexandr.kramar@cyberiansoft.com>
2016-10-19 17:29:21 +0300

    -Added object model for Devices Popup for iOS HD version client

---
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/ios_client/pageobjects/iosdevicescreens/DevicesPopupScreen.java
@@ -0,0 +1,36 @@
+package com.cyberiansoft.test.ios_client.pageobjects.iosdevicescreens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSFindBy;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class DevicesPopupScreen extends iOSHDBaseScreen {
+	
+	@iOSFindBy(uiAutomator = ".popover().navigationBar().buttons()[\"Assign\"]")
+    private IOSElement assignlbtn;
+	
+	
+	@iOSFindBy(uiAutomator = ".popover().navigationBar().buttons()[\"Cancel\"]")
+    private IOSElement cancelbtn;
+	
+	
+	public DevicesPopupScreen(AppiumDriver driver) {
+		super(driver);
+		PageFactory.initElements(new AppiumFieldDecorator(driver, 10, TimeUnit.SECONDS), this);
+		appiumdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	public void clickCancelButton() {
+		cancelbtn.click();
+	}
+	
+	public boolean isAssignButtonDisplayed() {
+		return assignlbtn.isDisplayed();
+	}
+
+}
