62a2ebd0198120a507df1b9b9dda653ab43c216c
Nikolay Lomeko <nikolay@166.104.120.77.colo.static.dcvolia.com>
2017-02-07 12:50:05 +0300

    - updated running script

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export JAVA_HOME=$(/usr/libexec/java_home)
+export ANDROID_HOME=/usr/local/Cellar/android-sdk/24.4.1_1
+
 export PATH=/opt/apache-maven-3.3.9/bin:$JAVA_HOME/bin:$PATH
+
 
 mvn clean install test -DsuiteXmlFile=hdcalculationstestcases10.xml
