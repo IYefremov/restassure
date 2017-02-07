8458c4f7dab9243491d4b9fe7c99a6b0a0d503ba
Nikolay Lomeko <nikolay@166.104.120.77.colo.static.dcvolia.com>
2017-02-07 12:41:40 +0300

    - updated running script

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export PATH=/opt/apache-maven-3.3.9/bin:$PATH
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH=/opt/apache-maven-3.3.9/bin:$JAVA_HOME/bin:$PATH
 
 mvn clean install test -DsuiteXmlFile=hdcalculationstestcases10.xml
