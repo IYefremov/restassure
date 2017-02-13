469974133c7e89a991991ab2a1b6e83e9b00d02e
Okramar <olexandr.kramar@cyberiansoft.com>
2017-02-13 14:53:21 +0200

    -Update object model for License screen

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/ios10_client/pageobjects/ioshddevicescreens/LicensesScreen.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/ios10_client/pageobjects/ioshddevicescreens/LicensesScreen.java
@@ -16,7 +16,7 @@ public class LicensesScreen extends iOSHDBaseScreen {
 	
 	public LicensesScreen(AppiumDriver driver) {
 		super(driver);
-		PageFactory.initElements(new AppiumFieldDecorator(driver, 10, TimeUnit.SECONDS), this);
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 		appiumdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
