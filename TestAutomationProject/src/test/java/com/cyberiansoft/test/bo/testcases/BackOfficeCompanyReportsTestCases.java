65efc898952e131770447115095d0c5e346f8fa7
Andrii <a.o.vozniuk@gmail.com>
2017-09-20 16:39:37 +0300

    New tests

---
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/bo/testcases/BackOfficeCompanyReportsTestCases.java
@@ -0,0 +1,55 @@
+package com.cyberiansoft.test.bo.testcases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.cyberiansoft.test.bo.pageobjects.webpages.BackOfficeHeaderPanel;
+import com.cyberiansoft.test.bo.pageobjects.webpages.BackOfficeLoginWebPage;
+import com.cyberiansoft.test.bo.pageobjects.webpages.ReportsWebPage;
+import com.cyberiansoft.test.bo.pageobjects.webpages.TechnicianCommissionsWebPage;
+
+public class BackOfficeCompanyReportsTestCases extends BaseTestCase {
+
+	@BeforeMethod
+	@Parameters({ "backoffice.url", "user.name", "user.psw" })
+	public void BackOfficeLogin(String backofficeurl,
+			String userName, String userPassword) throws InterruptedException {
+		try{
+		webdriverGotoWebPage(backofficeurl);
+		BackOfficeLoginWebPage loginpage = PageFactory.initElements(webdriver,
+				BackOfficeLoginWebPage.class);
+		loginpage.UserLogin(userName, userPassword);
+		Thread.sleep(2000);
+		}catch(Exception e){
+			BackOfficeLogout();
+			webdriverGotoWebPage(backofficeurl);
+			BackOfficeLoginWebPage loginpage = PageFactory.initElements(webdriver,
+					BackOfficeLoginWebPage.class);
+			loginpage.UserLogin(userName, userPassword);
+			Thread.sleep(2000);
+		}
+	}
+	
+	@AfterMethod
+	public void BackOfficeLogout() throws InterruptedException {
+		BackOfficeHeaderPanel backofficeheader = PageFactory.initElements(webdriver,
+				BackOfficeHeaderPanel.class);
+		backofficeheader.clickLogout();
+		Thread.sleep(3000);
+		
+	}
+	
+	@Test(testName = "Test Case 16189:Reports - Technician Commissions")
+	public void testReportsTechnicianCommissions() throws InterruptedException{
+		BackOfficeHeaderPanel backofficeheader = PageFactory.initElements(webdriver,
+				BackOfficeHeaderPanel.class);
+		ReportsWebPage reportsPage = backofficeheader.clickReportsLink();
+		TechnicianCommissionsWebPage technishialCommissionsPage = reportsPage.clickTechnicianCommissionsLink();
+		technishialCommissionsPage.clickSearchBTN();
+		technishialCommissionsPage.clickOnLastSearchResult();
+	}
+	
+}
