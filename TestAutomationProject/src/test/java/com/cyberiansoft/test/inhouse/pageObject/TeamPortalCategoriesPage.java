e58041b8d202d682fca03dc67916aa9784901163
Andrii <a.o.vozniuk@gmail.com>
2017-11-24 11:45:08 +0200

    inhouse page commit

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/inhouse/pageObject/TeamPortalCategoriesPage.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/inhouse/pageObject/TeamPortalCategoriesPage.java
@@ -1,13 +1,40 @@
 package com.cyberiansoft.test.inhouse.pageObject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class TeamPortalCategoriesPage extends  BasePage {
+import java.util.List;
+
+public class TeamPortalCategoriesPage extends BasePage {
+
+    @FindBy(xpath = "//div[@class='dropdown-toggle btn-add-category']")
+    WebElement addCategoryBTN;
+
+    @FindBy(className = "form-control")
+    List<WebElement> inputFields;
+
+    @FindBy(xpath = "//button[@class='submit btn-save-add-category']")
+    WebElement submitCategoryBTN;
+
     public TeamPortalCategoriesPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
 
 
-} 
+    public void clickAddCategoryButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(addCategoryBTN)).click();
+    }
+
+    public void setCategory(String category) throws InterruptedException {
+        Thread.sleep(1000);
+        inputFields.get(0).sendKeys(category);
+    }
+
+    public void clickSubmitCategoryButton(){
+        submitCategoryBTN.click();
+    }
+}
