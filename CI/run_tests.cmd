6db850c820c779ec7a789fda192bd834fb810028
Vladimir Avsievich <vladimir.avsievich@cyberiansoft.com>
2018-01-10 12:20:49 +0000

    Updated run_tests.cmd

--- CI/run_tests.cmd
+++ CI/run_tests.cmd
@@ -1,8 +1,8 @@
-# $1 - test file name
+# %1 - test file name
 
-#echo "Incoming parameters:"
-#echo $1
-#echo "------------------"
+echo "Incoming parameters:"
+echo %1
+echo "------------------"
 
 #export JAVA_HOME=$(/usr/libexec/java_home)
 #export ANDROID_HOME=/usr/local/Cellar/android-sdk/24.4.1_1
@@ -10,4 +10,4 @@
 #export PATH=/opt/apache-maven-3.3.9/bin:$JAVA_HOME/bin:$PATH
 
 
-mvn clean install test -DsuiteXmlFile=$1
+mvn clean install test -DsuiteXmlFile=%1
