94fa709e5b3ac7d8932119ade54d980ac1dc0634
OKramar <olexandr.kramar@cyberiansoft.com>
2018-03-29 11:41:22 +0300

    -Fixed imports in SR screen

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/ios10_client/pageobjects/ioshddevicescreens/ServiceRequestsScreen.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/ios10_client/pageobjects/ioshddevicescreens/ServiceRequestsScreen.java
@@ -1,6 +1,5 @@
 package com.cyberiansoft.test.ios10_client.pageobjects.ioshddevicescreens;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,10 +16,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import io.appium.java_client.pagefactory.iOSFindBy;
 
 import com.cyberiansoft.test.ios10_client.utils.Helpers;
 
@@ -356,7 +353,6 @@ public class ServiceRequestsScreen extends iOSHDBaseScreen {
 		//FluentWait<WebDriver> wait = new WebDriverWait(appiumdriver, 5);
 
 		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//XCUIElementTypeNavigationBar[@name='AppointmentsView']/XCUIElementTypeButton[@name='Add']"))).click(); 
-		System.out.println("+++" +  appiumdriver.findElementsByAccessibilityId("Add").size());
 		List<WebElement> addds = appiumdriver.findElementsByAccessibilityId("Add");
 		for(WebElement add : addds)
 			if (add.isDisplayed()) {
