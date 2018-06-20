c3a6d3a73034241b9a1c065fc174f87774c69569
OKramar <olexandr.kramar@cyberiansoft.com>
2018-06-20 18:12:48 +0300

    -R360 Remove app reset

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/BaseTestCaseTeamEditionRegistration.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/BaseTestCaseTeamEditionRegistration.java
@@ -1,14 +1,12 @@
 package com.cyberiansoft.test.vnext.testcases;
 
-import org.testng.annotations.BeforeTest;
-
 import com.cyberiansoft.test.dataclasses.Employee;
 import com.cyberiansoft.test.dataclasses.RetailCustomer;
 import com.cyberiansoft.test.dataclasses.WholesailCustomer;
 import com.cyberiansoft.test.dataprovider.JSonDataParser;
 import com.cyberiansoft.test.vnext.config.VNextTeamRegistrationInfo;
 import com.cyberiansoft.test.vnext.screens.VNextLoginScreen;
-import com.cyberiansoft.test.vnext.utils.VNextAppUtils;
+import org.testng.annotations.BeforeTest;
 
 public class BaseTestCaseTeamEditionRegistration extends VNextBaseTestCase {
 	
@@ -18,7 +16,7 @@ public class BaseTestCaseTeamEditionRegistration extends VNextBaseTestCase {
 	@BeforeTest
 	public void beforeTest() throws Exception {
 		setUp();	
-		VNextAppUtils.resetApp();
+		//VNextAppUtils.resetApp();
 		registerTeamEdition(VNextTeamRegistrationInfo.getInstance().getDeviceDefaultLicenseName());
 		Employee employee = JSonDataParser.getTestDataFromJson("src/test/java/com/cyberiansoft/test/vnext/data/team-device-employee.json", Employee.class);
 		VNextLoginScreen loginscreen = new VNextLoginScreen(appiumdriver);
