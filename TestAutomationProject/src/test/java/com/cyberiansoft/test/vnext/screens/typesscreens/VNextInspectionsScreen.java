2d9644e24d4afa07843e2ee5e85f332270c07760
OKramar <olexandr.kramar@cyberiansoft.com>
2018-09-05 00:03:00 +0300

    -Fix for isInspectionExists method

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/screens/typesscreens/VNextInspectionsScreen.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/screens/typesscreens/VNextInspectionsScreen.java
@@ -12,7 +12,6 @@ import com.cyberiansoft.test.vnext.utils.WaitUtils;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -167,16 +166,9 @@ public class VNextInspectionsScreen extends VNextBaseTypeScreen {
 	}
 	
 	public boolean isInspectionExists(String inspnumber) {
-		try {
-			WebDriverWait wait = new WebDriverWait(appiumdriver, 30);
-			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@class, 'checkbox-item-title') and text()='" + inspnumber + "']")));
-			return true;
-		} catch (NoSuchElementException e) {
-			//System.out.println("++++++++++++++" + e.getMessage());
-		}
-		System.out.println("++++++++++++++" + "//div[contains(@class, 'checkbox-item-title') and text()='" + inspnumber + "']");
-
-		return false;//inspectionslist.findElements(By.xpath(".//div[@class='checkbox-item-title' and text()='" + inspnumber + "']")).size() > 0;
+		WebDriverWait wait = new WebDriverWait(appiumdriver, 30);
+		wait.until(ExpectedConditions.visibilityOf(inspectionslist));
+		return inspectionslist.findElements(By.xpath(".//div[@class='checkbox-item-title' and text()='" + inspnumber + "']")).size() > 0;
 	}
 	
 	public void switchToTeamInspectionsView() {
