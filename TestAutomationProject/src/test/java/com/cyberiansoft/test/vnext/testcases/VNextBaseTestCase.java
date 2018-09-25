ea25f5a79cc5a5360283933e435ad999b986d22a
OKramar <olexandr.kramar@cyberiansoft.com>
2018-09-25 19:42:03 +0300

    -R360 increased timeouts

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/VNextBaseTestCase.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/VNextBaseTestCase.java
@@ -220,6 +220,7 @@ public class VNextBaseTestCase {
 
 		VNextTeamEditionVerificationScreen verificationscreen = new VNextTeamEditionVerificationScreen(DriverBuilder.getInstance().getAppiumDriver());
 		verificationscreen.setDeviceRegistrationCode(regCode);
+		BaseUtils.waitABit(3000);
 		verificationscreen.clickVerifyButton();
 
 		BaseUtils.waitABit(20*1000);
