33f494b74bde91c7fa395cceb6f99f9757b942cc
Vitaly Lyashenko <lyashenko@cyberiansoft.com>
2019-04-30 15:02:15 +0300

    mac5 fix

--- CI/run_test_Mac5.sh
+++ CI/run_test_Mac5.sh
@@ -9,7 +9,7 @@ echo "------------------"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export ANDROID_HOME=/Users/aqc/Library/android/sdk
 
-export PATH=/usr/local/Cellar/maven/3.6./bin:$JAVA_HOME/bin:$PATH
+export PATH=/usr/local/Cellar/maven/3.6.1/bin:$JAVA_HOME/bin:$PATH
 
 
 mvn clean install test -DsuiteXmlFile=$1
