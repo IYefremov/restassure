684688debb859d11cab635a6b044046e4edc8030
OKramar <olexandr.kramar@cyberiansoft.com>
2019-05-09 14:11:33 +0300

    #81381 - Create suite for ReconPro customer creation feature

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/BaseTestCaseWithDeviceRegistrationAndUserLogin.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/BaseTestCaseWithDeviceRegistrationAndUserLogin.java
@@ -1,12 +1,12 @@
 package com.cyberiansoft.test.vnext.testcases;
 
-import com.cyberiansoft.test.baseutils.AppiumUtils;
 import com.cyberiansoft.test.dataclasses.Employee;
 import com.cyberiansoft.test.dataprovider.JSonDataParser;
+import com.cyberiansoft.test.vnext.config.VNextConfigInfo;
 import com.cyberiansoft.test.vnext.config.VNextUserRegistrationInfo;
 import com.cyberiansoft.test.vnext.screens.VNextHomeScreen;
-import com.cyberiansoft.test.vnext.utils.VNextAppUtils;
-import org.testng.annotations.BeforeClass;
+import com.cyberiansoft.test.vnext.screens.VNextLoginScreen;
+import org.testng.annotations.BeforeTest;
 
 import java.io.File;
 
@@ -15,21 +15,18 @@ public class BaseTestCaseWithDeviceRegistrationAndUserLogin extends VNextBaseTes
 	final static String testEmployee = VNextUserRegistrationInfo.getInstance().getDeviceEmployeeName();
 	final static String testEmployeePsw = VNextUserRegistrationInfo.getInstance().getDeviceEmployeePassword();
 
-
-
-	@BeforeClass(description = "Setting up new suite")
+	@BeforeTest(description = "Setting up new suite")
 	public void settingUp() throws Exception {
 
 		setUp();
-		AppiumUtils.setNetworkOn();
-		VNextAppUtils.resetApp();
-		registerDevice();
-		//AppiumUtils.switchApplicationContext(AppContexts.NATIVE_CONTEXT);
-		//AppiumUtils.switchApplicationContext(AppContexts.WEBVIEW_CONTEXT);
+		if (VNextConfigInfo.getInstance().installNewBuild()) {
+			//VNextAppUtils.resetApp();
+			registerDevice();
 
-		employee = JSonDataParser.getTestDataFromJson(new File("src/test/java/com/cyberiansoft/test/vnext/data/free-device-employee.json"), Employee.class);
-		//VNextLoginScreen loginScreen = new VNextLoginScreen(appiumdriver);
-		//loginScreen.userLogin(employee.getEmployeeName(), employee.getEmployeePassword());
+			employee = JSonDataParser.getTestDataFromJson(new File("src/test/java/com/cyberiansoft/test/vnext/data/free-device-employee.json"), Employee.class);
+			VNextLoginScreen loginScreen = new VNextLoginScreen(appiumdriver);
+			loginScreen.userLogin(employee.getEmployeeName(), employee.getEmployeePassword());
+		}
 		new VNextHomeScreen(appiumdriver);
 	}
 }
--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/vNextCustomersTestCases.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/testcases/vNextCustomersTestCases.java
@@ -1,11 +1,16 @@
 package com.cyberiansoft.test.vnext.testcases;
 
 
+import com.cyberiansoft.test.baseutils.StringUtils;
+import com.cyberiansoft.test.dataprovider.JSONDataProvider;
+import com.cyberiansoft.test.dataprovider.JSonDataParser;
 import com.cyberiansoft.test.vnext.screens.*;
 import com.cyberiansoft.test.vnext.screens.customers.VNextCustomersScreen;
 import com.cyberiansoft.test.vnext.screens.menuscreens.VNextCustomersMenuScreen;
+import org.json.simple.JSONObject;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.cyberiansoft.test.baseutils.AppiumUtils;
@@ -20,24 +25,26 @@ import com.cyberiansoft.test.vnext.config.VNextConfigInfo;
 
 
 public class vNextCustomersTestCases extends BaseTestCaseWithDeviceRegistrationAndUserLogin {
+
+	private static final String DATA_FILE = "src/test/java/com/cyberiansoft/test/vnext/data/customers-testcases-data.json";
+
+
+	@BeforeClass(description="Team Customers Test Cases")
+	public void beforeClass() {
+		JSONDataProvider.dataFile = DATA_FILE;
+	}
 	
 	/*@AfterSuite
 	public void deleteClients() throws IOException {
 		VNextWebServicesUtils.deleteClientsByMail("cnolan@gmail.com");
 	}*/
-	
-	@Test(testName= "Test Case 43519:vNext - Create new Customer with empty First Name and Last Name", 
-			description = "Create new Customer with empty First Name and Last Name")
-	public void testCreateNewCustomerWithEmptyFirstNameAndLastName() throws InterruptedException {
-		
-		final RetailCustomer testcustomer = new RetailCustomer();
-		testcustomer.setMailAddress("");
-		testcustomer.setCompanyName("AquaAuto");
-		testcustomer.setCustomerAddress1("Test address street, 1");
-		testcustomer.setCustomerPhone("444-51-09");
-		testcustomer.setCustomerCountry("Mexico");
-		testcustomer.setCustomerState("Colima");
-		
+
+	@Test(dataProvider="fetchData_JSON", dataProviderClass=JSONDataProvider.class)
+	public void testCreateNewCustomerWithEmptyFirstNameAndLastName(String rowID,
+																   String description, JSONObject testData) {
+
+		final RetailCustomer testcustomer = JSonDataParser.getTestDataFromJson(testData, RetailCustomer.class);
+
 		deleteCustomerOnBackOffice(testcustomer.getCompany(), "");
 
 		VNextHomeScreen homescreen = new VNextHomeScreen(appiumdriver);
@@ -53,14 +60,13 @@ public class vNextCustomersTestCases extends BaseTestCaseWithDeviceRegistrationA
 		Assert.assertEquals(newcustomerscreen.getCustomerCompanyName(), testcustomer.getCompany());
 		Assert.assertEquals(newcustomerscreen.getCustomerEmail(), testcustomer.getMailAddress());
 		Assert.assertEquals(newcustomerscreen.getCustomerPhone(), testcustomer.getCustomerPhone());
-		Assert.assertEquals(newcustomerscreen.getCustomerAddress(), upperCaseAllFirst(testcustomer.getCustomerAddress1()));
+		Assert.assertEquals(newcustomerscreen.getCustomerAddress(), StringUtils.upperCaseAllFirst(testcustomer.getCustomerAddress1()));
 		Assert.assertEquals(newcustomerscreen.getCustomerCountry(), testcustomer.getCustomerCountry());
 		Assert.assertEquals(newcustomerscreen.getCustomerState(), testcustomer.getCustomerState());
 		customersscreen = newcustomerscreen.clickBackButton();
 		customersscreen.clickBackButton();
-		homescreen = new VNextHomeScreen(appiumdriver);
 		BaseUtils.waitABit(45000);
-		
+
 		webdriver = WebdriverInicializator.getInstance().initWebDriver(browsertype);
 		webdriver.get(VNextConfigInfo.getInstance().getBackOfficeVnextDevURL());
 		BackOfficeLoginWebPage loginpage = PageFactory.initElements(webdriver,
@@ -77,21 +83,15 @@ public class vNextCustomersTestCases extends BaseTestCaseWithDeviceRegistrationA
 		Assert.assertFalse(clientspage.isClientPresentInTable(testcustomer.getCompany()));
 		webdriver.quit();
 	}
-	
-	@Test(testName= "Test Case 43522:vNext - Verify customer created in Offline mode is available after DB update", 
-			description = "Verify customer created in Offline mode is available after DB update")
-	public void testVerifyCustomerCreatedInOfflineModeIsAvailableAfterDBUpdate() {
-		
-		final RetailCustomer testcustomer = new RetailCustomer("Christofer", "Nolan");
-		testcustomer.setMailAddress("cnolan@gmail.com");
-		testcustomer.setCompanyName("Toyota-Diamant");
-		testcustomer.setCustomerAddress1("");
-		testcustomer.setCustomerPhone("032-449-56");
-		testcustomer.setCustomerCountry("Germany");
-		testcustomer.setCustomerState("Saarland");
-		
+
+	@Test(dataProvider="fetchData_JSON", dataProviderClass=JSONDataProvider.class)
+	public void testVerifyCustomerCreatedInOfflineModeIsAvailableAfterDBUpdate(String rowID,
+																			   String description, JSONObject testData) {
+
+		final RetailCustomer testcustomer = JSonDataParser.getTestDataFromJson(testData, RetailCustomer.class);
+
 		deleteCustomerOnBackOffice(testcustomer.getFirstName(), testcustomer.getLastName());
-		
+
 		VNextHomeScreen homescreen = new VNextHomeScreen(appiumdriver);
 		AppiumUtils.setNetworkOff();
 		VNextCustomersScreen customersscreen = homescreen.clickCustomersMenuItem();
@@ -117,21 +117,14 @@ public class vNextCustomersTestCases extends BaseTestCaseWithDeviceRegistrationA
 		Assert.assertEquals(newcustomerscreen.getCustomerState(), testcustomer.getCustomerState());
 		customersscreen = newcustomerscreen.clickBackButton();
 		customersscreen.clickBackButton();
-		homescreen = new VNextHomeScreen(appiumdriver);
 	}
-	
-	@Test(testName= "vNext - Verify new customer is available after DB update", 
-			description = "Verify new customer is available after DB update")
-	public void testVerifyNewCustomerIsAvailableAfterDBUpdate() {
-		
-		final RetailCustomer testcustomer = new RetailCustomer("Test", "DBUpdateCustomer");
-		testcustomer.setMailAddress("cnolan@gmail.com");
-		testcustomer.setCompanyName("Toyota-Diamant");
-		testcustomer.setCustomerAddress1("");
-		testcustomer.setCustomerPhone("032-449-56");
-		testcustomer.setCustomerCountry("Germany");
-		testcustomer.setCustomerState("Saarland");
-		
+
+	@Test(dataProvider="fetchData_JSON", dataProviderClass=JSONDataProvider.class)
+	public void testVerifyNewCustomerIsAvailableAfterDBUpdate(String rowID,
+															  String description, JSONObject testData) {
+
+		final RetailCustomer testcustomer = JSonDataParser.getTestDataFromJson(testData, RetailCustomer.class);
+
 		deleteCustomerOnBackOffice(testcustomer.getFirstName(), testcustomer.getLastName());
 		
 		VNextHomeScreen homescreen = new VNextHomeScreen(appiumdriver);
@@ -157,10 +150,9 @@ public class vNextCustomersTestCases extends BaseTestCaseWithDeviceRegistrationA
 		Assert.assertEquals(newcustomerscreen.getCustomerState(), testcustomer.getCustomerState());
 		customersscreen = newcustomerscreen.clickBackButton();
 		customersscreen.clickBackButton();
-		homescreen = new VNextHomeScreen(appiumdriver);
 	}
 	
-	public void deleteCustomerOnBackOffice(String firstName, String lastName) {
+	private void deleteCustomerOnBackOffice(String firstName, String lastName) {
 		
 		String customerDelete = "";
 		
@@ -186,19 +178,5 @@ public class vNextCustomersTestCases extends BaseTestCaseWithDeviceRegistrationA
 		Assert.assertFalse(clientspage.isClientPresentInTable(customerDelete));
 		webdriver.quit();
 	}
-	
-	public String upperCaseAllFirst(String value) {
-
-        char[] array = value.toCharArray();
-        array[0] = Character.toUpperCase(array[0]);
-
-        // Uppercase all letters that follow a whitespace character.
-        for (int i = 1; i < array.length; i++) {
-            if (Character.isWhitespace(array[i - 1])) {
-                array[i] = Character.toUpperCase(array[i]);
-            }
-        }
-        return new String(array);
-    }
 
 }
