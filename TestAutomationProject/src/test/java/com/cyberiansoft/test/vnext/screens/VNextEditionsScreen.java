dc353cf6d8825f16ac9689fe7e67bd16b8ed0c02
OKramar <olexandr.kramar@cyberiansoft.com>
2019-07-12 11:01:36 +0300

    -R360: Fix for select edition

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/screens/VNextEditionsScreen.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/screens/VNextEditionsScreen.java
@@ -4,6 +4,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import lombok.Getter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -23,7 +24,7 @@ public class VNextEditionsScreen extends VNextBaseScreen {
     }
 
     public VNextEnvironmentSelectionScreen selectEdition(String editionName) {
-        //tap(editionslist.findElement(By.xpath(".//div[contains(text(), '" + editionName + "')]")));
+        tap(editionslist.findElement(By.xpath(".//div[contains(text(), '" + editionName + "')]")));
         return new VNextEnvironmentSelectionScreen(appiumdriver);
     }
 }
