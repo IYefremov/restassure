17a50c4d26d79eea1711bb12f7b629df44fab8ab
Yevhen Shabalin <yshabalin@cyberiansoft.com>
2019-08-27 16:10:40 +0300

    86717: added method for paths that would be safe to use from both windows and mac os

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/baseutils/OsUtils.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/baseutils/OsUtils.java
@@ -1,16 +1,29 @@
 package com.cyberiansoft.test.baseutils;
 
 public class OsUtils {
-    private static String OS = null;
+	private static String OS = null;
 
-    public static String getOsName() {
-        if (OS == null) {
-            OS = System.getProperty("os.name");
-        }
-        return OS;
-    }
+	public static String getOsName() {
+		if (OS == null) {
+			OS = System.getProperty("os.name");
+		}
+		return OS;
+	}
 
-    public static boolean isWindows() {
-        return getOsName().startsWith("Windows");
-    }
+	public static boolean isWindows() {
+		return getOsName().startsWith("Windows");
+	}
+
+	public static boolean isMacOS() {
+		return getOsName().startsWith("Mac");
+	}
+
+	//TODO test *nix systems
+	public static String getOSSafePath(final String originalPath) {
+		if (isMacOS())
+			return String.format("%s%s%s", System.getProperty("user.dir"), "/../", originalPath);
+		else
+			return originalPath;
+
+	}
 }
