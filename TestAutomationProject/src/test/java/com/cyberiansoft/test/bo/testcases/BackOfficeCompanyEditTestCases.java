0cd833a55b9d89b52d380a4321d98099e434a745
Iryna Boboshko <Boboshko@cyberiansoft.com>
2019-09-19 02:42:02 -0700

    Merged PR 9252: BO: configuring the project to be run on the Azure machine
    BO: configuring the project to be run on the Azure machine

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/bo/testcases/BackOfficeCompanyEditTestCases.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/bo/testcases/BackOfficeCompanyEditTestCases.java
@@ -6,7 +6,6 @@ import com.cyberiansoft.test.dataprovider.JSONDataProvider;
 import com.cyberiansoft.test.dataprovider.JSonDataParser;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
