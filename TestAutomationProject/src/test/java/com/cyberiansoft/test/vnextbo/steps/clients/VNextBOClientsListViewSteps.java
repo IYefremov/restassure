3e15f0eaef7aea7e9a781f6e962451abb888d4b4
Irene Boboshko <boboshko@cyberiansoft.com>
2019-10-21 19:18:44 +0300

    R360 BO: page object in step methods removed

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnextbo/steps/clients/VNextBOClientsListViewSteps.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnextbo/steps/clients/VNextBOClientsListViewSteps.java
@@ -1,16 +1,12 @@
 package com.cyberiansoft.test.vnextbo.steps.clients;
 
-import com.cyberiansoft.test.driverutils.DriverBuilder;
 import com.cyberiansoft.test.vnextbo.interactions.clients.VNextBOClientsListViewInteractions;
-import com.cyberiansoft.test.vnextbo.screens.clients.clientDetails.VNextBOClientsDetailsViewAccordion;
-import org.openqa.selenium.support.PageFactory;
 
 public class VNextBOClientsListViewSteps {
 
-    public static VNextBOClientsDetailsViewAccordion openClientsDetailsPage(String client) {
+    public static void openClientsDetailsPage(String client) {
         final VNextBOClientsListViewInteractions clientsListViewInteractions = new VNextBOClientsListViewInteractions();
         clientsListViewInteractions.clickActionsIcon(client);
         clientsListViewInteractions.clickEditDropMenuButton();
-        return PageFactory.initElements(DriverBuilder.getInstance().getDriver(), VNextBOClientsDetailsViewAccordion.class);
     }
 }
