61f593147dcfb6f81031e37a36c5e159b8c31be5
kramar <olexandr.kramar@cyberiansoft.com>
2020-02-07 19:03:23 +0200

    -R360: new Monitor Filters test cases 92305, 92301, 92297 -R360: test cases fixes

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/validations/MonitorSearchValidations.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/validations/MonitorSearchValidations.java
@@ -3,6 +3,7 @@ package com.cyberiansoft.test.vnext.validations;
 import com.cyberiansoft.test.baseutils.CustomDateProvider;
 import com.cyberiansoft.test.enums.DateUtils;
 import com.cyberiansoft.test.enums.OrderPriority;
+import com.cyberiansoft.test.vnext.enums.RepairOrderStatus;
 import com.cyberiansoft.test.vnext.screens.monitoring.CommonFilterScreen;
 import com.cyberiansoft.test.vnext.screens.monitoring.RepairOrderScreen;
 import com.cyberiansoft.test.vnext.utils.WaitUtils;
@@ -45,5 +46,23 @@ public class MonitorSearchValidations {
         CommonFilterScreen commonFilterScreen = new CommonFilterScreen();
         Assert.assertEquals(commonFilterScreen.getPriority().getRootElement().getAttribute("value"),
                 StringUtils.capitalize(orderPriority.getValue()));
+    }
+
+    public static void validateStatusValue(RepairOrderStatus repairOrderStatus) {
+        CommonFilterScreen commonFilterScreen = new CommonFilterScreen();
+        Assert.assertEquals(commonFilterScreen.getStatus().getRootElement().getAttribute("value"),
+                repairOrderStatus.getStatusString());
+    }
+
+    public static void validatePhaseValue(String phaseName) {
+        CommonFilterScreen commonFilterScreen = new CommonFilterScreen();
+        Assert.assertEquals(commonFilterScreen.getPhase().getRootElement().getAttribute("value"),
+                phaseName);
+    }
+
+    public static void validateDepartmentValue(String departmentName) {
+        CommonFilterScreen commonFilterScreen = new CommonFilterScreen();
+        Assert.assertEquals(commonFilterScreen.getDepartment().getRootElement().getAttribute("value"),
+                departmentName);
     }
 }
