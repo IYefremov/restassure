963b858dea2b88b62e90b66126b9d636d63e20d9
Oleksandr Nikitchuk <onikitchuk@gloriumtech.com>
2020-03-17 16:15:06 +0200

    91706 && 91718 finish

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/screens/VNextDeclineReasonScreen.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/screens/VNextDeclineReasonScreen.java
@@ -1,22 +1,31 @@
 package com.cyberiansoft.test.vnext.screens;
 
+import com.cyberiansoft.test.driverutils.ChromeDriverProvider;
+import com.cyberiansoft.test.vnext.webelements.decoration.FiledDecorator;
+import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+@Getter
 public class VNextDeclineReasonScreen extends VNextBaseScreen {
 	
 	@FindBy(xpath="//div[@data-page='services-add']")
-	private WebElement approveservicesscreen;
+	private WebElement approveServicesScreen;
 
 	public VNextDeclineReasonScreen() {
-		PageFactory.initElements(appiumdriver, this);
+		PageFactory.initElements(new FiledDecorator(ChromeDriverProvider.INSTANCE.getMobileChromeDriver()), this);
 	}
 
 	public void selectDeclineReason(String declineReason) {
-		tap(approveservicesscreen.findElement(By.
+		tap(approveServicesScreen.findElement(By.
 				xpath(".//ul/li[@action='select-item']/span[text()='" + declineReason + "']")));
+	}
+
+
+	public String getScreenText () {
+		return approveServicesScreen.findElement(By.
+				xpath("//div[@class='navbar-panel-inner']")).getText();
 	}
 
 }
