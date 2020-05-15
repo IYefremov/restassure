7669d8305615cc25bcf8119e6f553c21787bbf5f
kramar <olexandr.kramar@cyberiansoft.com>
2020-05-15 19:17:32 +0300

    -R360 client: Fix for Approve inspections

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/steps/ApproveSteps.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/steps/ApproveSteps.java
@@ -1,8 +1,11 @@
 package com.cyberiansoft.test.vnext.steps;
 
+import com.cyberiansoft.test.driverutils.ChromeDriverProvider;
 import com.cyberiansoft.test.vnext.interactions.ListSelectPageInteractions;
 import com.cyberiansoft.test.vnext.screens.VNextApproveScreen;
 import com.cyberiansoft.test.vnext.utils.WaitUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class  ApproveSteps {
 
@@ -14,6 +17,9 @@ public class  ApproveSteps {
     public static void saveApprove() {
         ListSelectPageInteractions.saveListPage();
         WaitUtils.waitLoadDialogDisappears();
+        WaitUtils.getGeneralFluentWait().until(ExpectedConditions.invisibilityOf(
+                ChromeDriverProvider.INSTANCE.getMobileChromeDriver().findElement(By.xpath("//div[@class='notifier-contaier']"))
+        ));
     }
 
     public static void clickClearSignatureButton() {
