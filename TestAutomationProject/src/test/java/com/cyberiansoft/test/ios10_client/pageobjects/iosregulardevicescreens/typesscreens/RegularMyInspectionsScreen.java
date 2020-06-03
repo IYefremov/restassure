15edd0a714865005300f83621c45c4a1a97075ce
Okramar <olexandr.kramar@cyberiansoft.com>
2020-06-03 18:48:44 +0300

    -ReconPro iOS: TCs Fixes

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/ios10_client/pageobjects/iosregulardevicescreens/typesscreens/RegularMyInspectionsScreen.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/ios10_client/pageobjects/iosregulardevicescreens/typesscreens/RegularMyInspectionsScreen.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class RegularMyInspectionsScreen extends RegularBaseTypeScreenWithTabs {
 
 	private final TypeScreenContext INSPECTIONCONTEXT = TypeScreenContext.INSPECTION;
