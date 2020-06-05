9e45d2e3e6a1f1838f0482adfc62b509f17bd78f
Yevhen Shabalin <yshabalin@cyberiansoft.com>
2020-06-05 14:07:59 +0300

    API calls for monitor BO settings changing

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/restclient/RestClient.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/vnext/restclient/RestClient.java
@@ -1,15 +1,26 @@
 package com.cyberiansoft.test.vnext.restclient;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class RestClient {
 
     private static Retrofit retrofit = null;
 
     public static Retrofit getRestClient(String baseUrl) {
-        if (retrofit==null) {
+
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+                    .client(okHttpClient)
                     .baseUrl(baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
