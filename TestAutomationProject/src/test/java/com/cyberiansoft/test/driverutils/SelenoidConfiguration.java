b1e922ee75ff56a07797b1d5b0655206f13e3a05
kramar <olexandr.kramar@cyberiansoft.com>
2020-06-24 16:09:06 +0300

    -R360 client: sessionTimeout fix

--- TestAutomationProject/src/test/java/com/cyberiansoft/test/driverutils/SelenoidConfiguration.java
+++ TestAutomationProject/src/test/java/com/cyberiansoft/test/driverutils/SelenoidConfiguration.java
@@ -1,6 +1,5 @@
 package com.cyberiansoft.test.driverutils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,10 +8,10 @@ public class SelenoidConfiguration {
     public DesiredCapabilities getCapabilities(ChromeOptions selenoidChromeOptions) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
-        capabilities.setVersion(WebDriverManager.chromedriver().getDownloadedVersion().substring(0, 4));
+        capabilities.setVersion("83.0");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", false);
-        capabilities.setCapability("sessionTimeout", "2m");
+        capabilities.setCapability("sessionTimeout", "8m");
         capabilities.setCapability("name", "SessionName");
         capabilities.setCapability(ChromeOptions.CAPABILITY, selenoidChromeOptions);
         return capabilities;
