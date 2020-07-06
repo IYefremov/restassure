30b60b13cb7c43db8900f447e0a51edcbabb2cbe
IvanYefremov <iyefremov@yahoo.com>
2020-07-06 15:10:58 +0300

    RestAssureProject

---
+++ Assure/src/test/java/ContractValidate/ContractTest.java
@@ -0,0 +1,31 @@
+package ContractValidate;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.RestAssured.given;
+
+
+public class ContractTest {
+
+    @Test
+    public void restGet() {
+        given()
+                .baseUri("https://reqres.in")
+                .contentType(ContentType.JSON)
+                .basePath("/api/users")
+                .param("page", 2)
+         //       .basePath("/api/users?page=2")
+
+                .when()
+                .get()
+                .then()
+                .log()
+                .body()
+                .body(matchesJsonSchemaInClasspath("usersResponseSchema.json"));
+
+    }
+}
+
+
