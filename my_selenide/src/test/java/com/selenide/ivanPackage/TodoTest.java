2e3191e03da5727824c0316b0a19186074405d31
IvanYefremov <iyefremov@yahoo.com>
2020-07-08 12:10:52 +0300

    Completed_CSS

--- my_selenide/src/test/java/com/selenide/ivanPackage/TodoTest.java
+++ my_selenide/src/test/java/com/selenide/ivanPackage/TodoTest.java
@@ -1,28 +1,25 @@
 package com.selenide.ivanPackage;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-
-public class toDoTest {
+public class TodoTest {
     @Test
     void completeTask() {
         open("http://todomvc.com/examples/emberjs/");
         element("#new-todo").setValue("a").pressEnter();
         element("#new-todo").setValue("b").pressEnter();
         element("#new-todo").setValue("c").pressEnter();
+
         // check a, b, c elements were added
         elements("#todo-list>li").shouldHave(exactTexts("a", "b", "c"));
         // toggle b item
         element("ul[id='todo-list'] li:nth-child(2) .toggle").click();
         // verify completed task should be b
         element("ul[id='todo-list'] li.completed").shouldBe(exactText("b"));
+
         // verify active task should be a and c
         elements("#todo-list li:not( .completed)").shouldHave(exactTexts("a", "c"));
     }
